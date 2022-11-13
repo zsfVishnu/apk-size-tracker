a09760de01a1f66d8575a67f727353a1c11da3a1
shrutiverma29 <shrutiv.verma9@gmail.com>
2022-11-13 12:47:45 +0530

    feat: test

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ import { getInput, setOutput, setFailed } from '@actions/core';
 import { context } from '@actions/github';
 import { execSync } from 'child_process';
 import { Octokit } from "octokit";
+import axios from 'axios'
+import AdmZip from 'adm-zip'
+
 
 const core = require('@actions/core');
 const github = require('@actions/github');
@@ -27,37 +30,101 @@ try {
     const owner = context.repo.owner
     const repo = context.repo.repo
 
-    console.log("Owner")
-    console.log(owner)
-    console.log("Repo")
-    console.log(repo)
+    // console.log("Owner")
+    // console.log(owner)
+    // console.log("Repo")
+    // console.log(repo)
 
 
-    // console.log('before exec')
-    execSync(' curl -L \
-    -H "Accept: application/vnd.github+json" \
-    -H "Authorization: Bearer $GITHUB_TOKEN" \
-    https://api.github.com/repos/$owner/$repo/actions/artifacts/428930352/zip -o a.zip', { encoding: 'utf8', maxBuffer: 50 * 1024 * 1024 })
+    // // console.log('before exec')
+    // execSync(' curl -L \
+    // -H "Accept: application/vnd.github+json" \
+    // -H "Authorization: Bearer $GITHUB_TOKEN" \
+    // https://api.github.com/repos/$owner/$repo/actions/artifacts/428930352/zip -o a.zip', { encoding: 'utf8', maxBuffer: 50 * 1024 * 1024 })
 
 
 
-    console.log(execSync(
-        'ls',
-        { encoding: 'utf8', maxBuffer: 50 * 1024 * 1024 }
-    ).toString());
-    console.log('after exec')
+    // console.log(execSync(
+    //     'ls',
+    //     { encoding: 'utf8', maxBuffer: 50 * 1024 * 1024 }
+    // ).toString());
+    // console.log('after exec')
 
 
     // console.log(execSync('unzip a.zip', { encoding: 'utf8', maxBuffer: 50 * 1024 * 1024 }))
-    execSync('ditto -x -k a.zip ./')
+    // // execSync('ditto -x -k a.zip ./')
 
-    const loadJSON = (path) => JSON.parse(fs.readFileSync(new URL(path, import.meta.url)));
+    // const loadJSON = (path) => JSON.parse(fs.readFileSync(new URL(path, import.meta.url)));
 
-    const stats = loadJSON('sizeArtifact.json');
+    // const stats = loadJSON('sizeArtifact.json');
 
-    console.log(stats)
+    // console.log(stats)
 
-    console.log("%%%%%%%%%%%%%%%%%%%%%%")
+    // console.log("%%%%%%%%%%%%%%%%%%%%%%")
+
+
+
+
+    async function makeRequest() {
+
+        const config = {
+            method: 'GET',
+            url: 'https://api.github.com/repos/zsfVishnu/ModernApp/actions/artifacts',
+            headers: {
+                'accept': 'application/vnd.github+json',
+                'authorization': 'Bearer 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'
+            }
+        }
+
+        let res = await axios(config)
+
+        // console.log(res.request._header);
+        // console.log(res.data.artifacts)
+        // console.log(res.data.artifacts.length)
+        const red_url = res.data.artifacts[0].archive_download_url
+
+        const config2 = {
+            method: 'GET',
+            url: red_url,
+            headers: {
+                'accept': 'application/vnd.github+json',
+                'authorization': 'Bearer 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'
+            },
+            responseType: "arraybuffer"
+        }
+
+        let res2 = await axios(config2)
+        // console.log(res2.request._header);
+        // console.log(res2)
+        // console.log(res2.data)
+
+
+
+        var zip = new AdmZip(res2.data);
+        var zipEntries = zip.getEntries();
+
+        console.log(zip)
+        console.log("***")
+        console.log(zip.getEntries()[0])
+        console.log("***")
+        console.log(zip.getEntries().length)
+        console.log(zip.readAsText(zipEntries[0]))
+
+        // search for "index.html" which should be there
+        for (var i = 0; i < zipEntries.length; i++) {
+            console.log(zip.readAsText(zipEntries[i]));
+        }
+
+        // and to extract it into current working directory
+        // zip.extractAllTo('.', true);
+
+
+    }
+
+
+    makeRequest();
+
+
 
 } catch (error) {
     setFailed(error.message);

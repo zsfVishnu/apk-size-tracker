73b890def317f6ad6124f4291c2be5fd157efa96
zsfVishnu <vishnu.tp32@gmail.com>
2022-11-23 00:11:17 +0530

    feat: payload formatting

--- evaluator.js
+++ evaluator.js
@@ -20,12 +20,14 @@ export function getFeatureBranchSize(flavorToBuild, buildPath) {
 export function getDeltaPayload(masterSize, featSize) {
   const delta = masterSize - featSize;
   const del = delta < 0 ? "Increase" : "Decrease";
-  const payload = ` | Info                       | Number               |
-                    | ---------------------------|--------------------- |
-                    | master branch size (in MB) | ${masterSize / 1024} |
-                    | feature branch size (in MB)| ${featSize / 1024}   |
-                    | ${del} in size             | ${Math.abs(delta)} KB |
-                    | ${del} in size             | ${Math.abs(delta) / 1024} MB |`;
+  const payload = `
+
+    | Info  | Value |
+    | ------------- | ------------- |
+    | Master branch size (in MB) | ${masterSize / 1024}  |
+    | Feature branch size (in MB)  | ${featSize / 1024} |
+    | ${del} in size  (in KB)  | ${Math.abs(delta)} |
+    | ${del} in size  (in MB)  | ${Math.abs(delta) / 1024} | `;
 
   return payload;
 }

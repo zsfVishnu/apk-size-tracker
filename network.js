339937edfb3ecf7212253e70028b56b21b8723fd
Yasir <yasir.ansari@dream11.com>
2023-06-19 13:00:09 +0530

    chore: added single metric file changes

--- network.js
+++ network.js
@@ -6,7 +6,7 @@ import { noArtifactFoundError } from "./error";
 export async function getMasterSizeFromArtifact(GITHUB_TOKEN, metricType) {
   const config = {
     method: "GET",
-    url: `https://api.github.com/repos/${context.repo.owner}/${context.repo.repo}/actions/artifacts?name=metric-artifact`,
+    url: `https://api.github.com/repos/${context.repo.owner}/${context.repo.repo}/actions/artifacts?name=metric-artifact-new`,
     headers: {
       accept: "application/vnd.github+json",
       authorization: "Bearer " + GITHUB_TOKEN,
@@ -21,7 +21,7 @@ export async function getMasterSizeFromArtifact(GITHUB_TOKEN, metricType) {
     for (let i = 0; i < artifacts.length; i++) {
       const red_url = artifacts[i].archive_download_url;
       console.log("Artifact name :: ", artifacts[i].name)
-      if (artifacts[i].name === 'metric-artifact') {
+      if (artifacts[i].name === 'metric-artifact-new') {
         const config2 = {
           method: "GET",
           url: red_url,
@@ -37,13 +37,13 @@ export async function getMasterSizeFromArtifact(GITHUB_TOKEN, metricType) {
         var zipEntries = zip.getEntries();
         for (let i = 0; i < zipEntries.length; i++) {
           console.log('Zip entry name ::', zipEntries[i].entryName)
-          if (metricType === 'apk' && zipEntries[i].entryName === `apk-metric.json`) {
-            console.log('APK SIZE ::', JSON.parse(zip.readAsText(zipEntries[i]))[`master_size`])
-            return JSON.parse(zip.readAsText(zipEntries[i]))[`master_size`];
+          if (metricType === 'apk' && zipEntries[i].entryName === `metric.json`) {
+            console.log('APK SIZE ::', JSON.parse(zip.readAsText(zipEntries[i]))[`apk_size`])
+            return JSON.parse(zip.readAsText(zipEntries[i]))[`apk_size`];
           }
-          if (metricType === 'bundle' && zipEntries[i].entryName === `bundle-metric.json`) {
-            console.log('BUNDLE SIZE ::', JSON.parse(zip.readAsText(zipEntries[i]))[`master_size`])
-            return JSON.parse(zip.readAsText(zipEntries[i]))[`master_size`];
+          if (metricType === 'bundle' && zipEntries[i].entryName === `metric.json`) {
+            console.log('BUNDLE SIZE ::', JSON.parse(zip.readAsText(zipEntries[i]))[`bundle_size`])
+            return JSON.parse(zip.readAsText(zipEntries[i]))[`bundle_size`];
           }
         }
       }
